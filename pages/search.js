ca2318ce67c845734ff81f8f790bee5939ca3192
Rokas <rookas.rudzenskas@gmail.com>
2021-05-26 09:38:20 +0300

    Rokas

--- pages/search.js
+++ pages/search.js
@@ -3,17 +3,20 @@ import Head from "next/head";
 import Header from "../components/Header";
 import {API_KEY, CONTEXT_KEY} from "../keys";
 import response from "../response";
+import {useRouter} from "next/router";
+import SearchResults from "../components/SearchResults";
 
 const Search = ({results}) => {
     console.log(results)
+    const router = useRouter();
     return (
         <div>
             <Head>
-                <title>Search Results</title>
+                <title>{router.query.term} - Google Search</title>
             </Head>
 
             <Header />
-
+            <SearchResults results={results} />
         </div>
     );
 };
@@ -23,9 +26,10 @@ export default Search;
 export async function getServerSideProps(context) {
     // getting data from object
     const useDummyData = true;
+    const startIndex = context.query.start || "0";
 
     // forming request url
-    const data = useDummyData ? response :  await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}`).then(response => response.json());
+    const data = useDummyData ? response :  await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`).then(response => response.json());
     // alll of this stuff is in server working
     // pass the results to the front end
     // after the server has rendered.. pass the results to the client
