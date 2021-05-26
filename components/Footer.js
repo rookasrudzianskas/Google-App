e55606b890cd1f28fd0ba22cfa88aff60d327235
Rokas <rookas.rudzenskas@gmail.com>
2021-05-26 07:21:26 +0300

    Rokas' g

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { GlobeIcon } from "@heroicons/react/solid";
+
+const Footer = () => {
+    return (
+        <footer className="">
+
+        </footer>
+    );
+};
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import Head from 'next/head'
 import Avatar from "../components/Avatar";
 import {BeakerIcon, ViewGridIcon} from '@heroicons/react/solid'
+import Image from "next/image";
+import {SearchIcon} from "@heroicons/react/outline";
+import { MicrophoneIcon } from "@heroicons/react/solid";
+import Footer from "../components/Footer";
+
+
 
 export default function Home() {
   return (
@@ -37,6 +43,32 @@ export default function Home() {
             </div>
         </header>
     {/*    Body */}
+
+        <form action="" className="flex flex-col items-center mt-44 flex-grow">
+            <Image
+                src="https://www.google.lt/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png" height={100} width={300}
+
+            />
+
+            <div
+                className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full
+          border border-gray-200  px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl"
+            >
+                <SearchIcon className="h-5 mr-3 text-gray-500" />
+                <input
+                    type="text"
+                    className="flex-grow focus:outline-none "
+                />
+                <MicrophoneIcon className="h-5" />
+            </div>
+
+            <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm: space-x-4">
+                <button className="btn">Google Search</button>
+                <button className="btn">I'm feeling Lucky</button>
+            </div>
+        </form>
+
+        <Footer />
     {/*    Footer   */}
 
     </div>
